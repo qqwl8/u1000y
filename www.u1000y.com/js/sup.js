73eeb59df34af120c5f9a2804730b176647df2bc
liulaotou <lb_yx@163.com>
2021-07-26 22:02:27 +0800

    1
    1

---
+++ www.u1000y.com/js/sup.js
@@ -0,0 +1,12 @@
+﻿$(document).ready(function(){		
+	//分类热门切换
+	$(".nav span").click(function(){
+		var page=$(this).attr("uid");//滑动门页面
+		//当前选项高亮
+		$(".nav span").removeClass();
+		$(this).addClass("hover");
+		//页面隐藏与现实
+		$("#news .list").hide();		
+		$(page).fadeIn(300);
+	});
+});
